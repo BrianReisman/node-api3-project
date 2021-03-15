1849ea646ce1568657e1dc6296de4c92d6472f75
BrianReisman <brianreisman@protonmail.com>
2021-03-15 13:43:13 -0400

    export and import all modules middleware and routes

--- api/server.js
+++ api/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
+const userRouter = require('./users/users-router'); //*the endpoint routes for /
+
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
-
+server.use(express.json())
 // global middlewares and the user's router need to be connected here
 
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>Let's write some middleware boissss!</h2>`);
 });
 
 module.exports = server;
